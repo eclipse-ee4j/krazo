101f8dab9adfb0cac840a1d9aa603fa8762e6b36
Gregor Tudan <gregor.tudan@cofinpro.de>
2019-02-04 22:12:54 +0100

    update license headers in files changed

--- core/src/main/java/org/eclipse/krazo/security/FormEntityProvider.java
+++ core/src/main/java/org/eclipse/krazo/security/FormEntityProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017, 2018 Ivar Grimstad
+ * Copyright © 2019 Eclipse Krazo committers and contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
