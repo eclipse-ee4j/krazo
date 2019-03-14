9dc06811f0d451eca123369855d26cc1d3c986d3
Erdle, Tobias <tobias.erdle@innoq.com>
2019-03-14 19:02:22 +0100

    Update copyright header
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- core/src/main/java/org/eclipse/krazo/engine/Viewable.java
+++ core/src/main/java/org/eclipse/krazo/engine/Viewable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017, 2018 Ivar Grimstad
+ * Copyright © 2017-2019 Ivar Grimstad
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
