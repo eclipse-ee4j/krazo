3b01694b37d48d08ca2b3c2b60ae6bfc260e55d8
Daniel Dias <daniel.dias.analistati@gmail.com>
2019-04-05 14:46:01 -0300

    update version docker Payara 5.191
    Signed-off-by: Daniel Dias <daniel.dias.analistati@gmail.com>

--- .travis/docker-payara.sh
+++ .travis/docker-payara.sh
@@ -3,4 +3,4 @@
 set -eu
 
 docker pull payara/server-web:5.191
-docker run --name=payara -d -p 8080:8080 -p 4848:4848 -it payara/server-web:5.183
+docker run --name=payara -d -p 8080:8080 -p 4848:4848 -it payara/server-web:5.191
