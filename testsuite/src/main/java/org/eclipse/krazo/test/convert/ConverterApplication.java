6e2728e4cd64cdc60bf895b7668c25d5d9dcbeef
Erdle, Tobias <tobias.erdle@innoq.com>
2019-12-16 09:47:05 +0100

    Move setup logic to WebListener to get test to run on TomEE
    see: #118
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- testsuite/src/main/java/org/eclipse/krazo/test/convert/ConverterApplication.java
+++ testsuite/src/main/java/org/eclipse/krazo/test/convert/ConverterApplication.java
@@ -1,44 +1,9 @@
 package org.eclipse.krazo.test.convert;
 
-import org.eclipse.krazo.binding.convert.ConverterRegistry;
-import org.eclipse.krazo.binding.convert.ConverterResult;
-import org.eclipse.krazo.binding.convert.MvcConverter;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.Locale;
 
 @ApplicationPath("resources")
 public class ConverterApplication extends Application {
 
-    @Inject
-    private ConverterRegistry converterRegistry;
-
-    @PostConstruct
-    public void init() {
-        converterRegistry.register(new AnswerToAllDoubleConverter());
-    }
-
-    /**
-     * Some really senseless converter to test the converter priority. It returns 42 on any run ;)
-     */
-    private static class AnswerToAllDoubleConverter implements MvcConverter<Double> {
-
-        @Override
-        public boolean supports(Class<Double> rawType) {
-            return Double.class.equals(rawType) || Double.TYPE.equals(rawType);
-        }
-
-        @Override
-        public ConverterResult<Double> convert(String value, Class<Double> rawType, Locale locale) {
-            return ConverterResult.success(42.0D);
-        }
-
-        @Override
-        public int getPriority() {
-            return 5;
-        }
-    }
 }
