04f3a69377cb4bab53a867dbd964a3f426842874
Erdle, Tobias <tobias.erdle@innoq.com>
2020-02-14 11:27:07 +0100

    Remove not working command
    see: #155
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- .travis/install-liberty.sh
+++ .travis/install-liberty.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-set -euo pipefail
-
 LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
 
 curl -L -s -o wlp.zip "${LIBERTY_URL}"
