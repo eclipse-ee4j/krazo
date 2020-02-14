64a196ea466fd83a1de94cf40f3ba1a17ff5cb95
Erdle, Tobias <tobias.erdle@innoq.com>
2020-02-14 15:33:32 +0100

    Try to export LIBERTY_HOME
    see: #155
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- .travis/install-liberty.sh
+++ .travis/install-liberty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
+export LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
 
 curl -L -s -o wlp.zip "${LIBERTY_URL}"
 unzip wlp.zip
