e20810e7dbba6034a394e132fd37e982d88326f6
Erdle, Tobias <tobias.erdle@innoq.com>
2020-02-14 15:46:43 +0100

    export the correct variable
    see: #155
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- .travis/install-liberty.sh
+++ .travis/install-liberty.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
+LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
 
 curl -L -s -o wlp.zip "${LIBERTY_URL}"
 unzip wlp.zip
 cp .travis/wlp-server-template.xml wlp/templates/servers/defaultServer/server.xml
-LIBERTY_HOME="$(cd ./wlp/ && pwd)"
+export  LIBERTY_HOME="$(cd ./wlp/ && pwd)"
