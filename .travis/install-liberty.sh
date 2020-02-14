ca233ad170c140483d32ab4930ba49f1ae9a045f
Erdle, Tobias <tobias.erdle@innoq.com>
2020-02-14 12:02:22 +0100

    Run liberty script with bash
    see: #155
    Signed-off-by: Erdle, Tobias <tobias.erdle@innoq.com>

--- .travis/install-liberty.sh
+++ .travis/install-liberty.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 LIBERTY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/19.0.0.11/wlp-webProfile8-19.0.0.11.zip"
 
@@ -6,4 +6,3 @@ curl -L -s -o wlp.zip "${LIBERTY_URL}"
 unzip wlp.zip
 cp .travis/wlp-server-template.xml wlp/templates/servers/defaultServer/server.xml
 LIBERTY_HOME="$(cd ./wlp/ && pwd)"
-
