dffc5b93f89c366b5419563b47ec8f8286cbe43a
Erdle, Tobias <tobi.erdle@gmail.com>
2021-04-30 22:03:51 +0200

    Set CSRF to implicit on generated application
    closes: #255

--- archetype/src/main/resources/archetype-resources/src/main/java/App.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,5 +1,10 @@
 package ${package};
 
+import java.util.Map;
+
+import jakarta.mvc.security.Csrf;
+import jakarta.mvc.security.Csrf.CsrfOptions;
+
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
@@ -8,4 +13,9 @@ import jakarta.ws.rs.core.Application;
  */
 @ApplicationPath("/mvc")
 public class App extends Application {
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return Map.of(Csrf.CSRF_PROTECTION, CsrfOptions.IMPLICIT);
+    }
 }
