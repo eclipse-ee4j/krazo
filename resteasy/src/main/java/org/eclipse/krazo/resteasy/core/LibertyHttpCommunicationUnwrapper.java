4b7e156ae55b692205d27a4388b8f5e5ce87d3c7
Jesse Gallagher <jesse@secondfoundation.org>
2021-12-15 08:31:27 -0500

    Fixed year in copyright header in LibertyHttpCommunicationUnwrapper

--- resteasy/src/main/java/org/eclipse/krazo/resteasy/core/LibertyHttpCommunicationUnwrapper.java
+++ resteasy/src/main/java/org/eclipse/krazo/resteasy/core/LibertyHttpCommunicationUnwrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Eclipse Krazo committers and contributors
+ * Copyright (c) 2021 Eclipse Krazo committers and contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
