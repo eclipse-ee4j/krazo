10da9de8eb8a94d3113c035aaba974a9eb38e39d
Erdle, Tobias <tobi.erdle@gmail.com>
2022-05-27 22:06:13 +0200

    Implement specified form method overwrite

--- testsuite/src/main/java/org/eclipse/krazo/test/forms/MyApplication.java
+++ testsuite/src/main/java/org/eclipse/krazo/test/forms/MyApplication.java
@@ -1,20 +1,8 @@
 package org.eclipse.krazo.test.forms;
 
-import org.eclipse.krazo.Properties;
-
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import java.util.HashMap;
-import java.util.Map;
 
 @ApplicationPath("resources")
 public class MyApplication extends Application {
-
-    @Override
-    public Map<String, Object> getProperties() {
-        final Map<String, Object> props = new HashMap<>();
-        props.put(Properties.HIDDEN_METHOD_FILTER_ACTIVE, true);
-
-        return props;
-    }
 }
